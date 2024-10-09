d1dcc06f3359bb08af84579eae48d9e1a3687a97
Boryana Dimitrova <b.dimitrova97@gmail.com>
2024-10-10 00:23:49 +0300

    create app for user list

--- Lectures/03.Components-Deep-Dive/04.Todo-List/client/src/App.jsx
+++ Lectures/03.Components-Deep-Dive/04.Todo-List/client/src/App.jsx
@@ -1,6 +1,3 @@
-import './App.css'
-import './components/Global.module.css';
-
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
