8978fb71a5439e3635591bf878ef984a008fb7d6
Boryana Dimitrova <b.dimitrova97@gmail.com>
2024-10-14 13:55:15 +0300

    Create useRef demo

--- Lectures/05.Forms/client/src/App.jsx
+++ Lectures/05.Forms/client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import UncontrolledForm from './components/UncontrolledForm'
 import ControlledForm from './components/ControlledForm'
+import RefDemo from './components/RefDemo'
 
 function App() {
   return (
     <>
       <UncontrolledForm/>
       <ControlledForm/>
+      <RefDemo/>
     </>
   )
 }
