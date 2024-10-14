b1ed876a197983f146dffce7b456d8e72e13f63a
Boryana Dimitrova <b.dimitrova97@gmail.com>
2024-10-14 16:01:17 +0300

    Install and setup taiwind

--- Lectures/05.Forms/client/src/App.jsx
+++ Lectures/05.Forms/client/src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css'
-import UncontrolledForm from './components/UncontrolledForm'
-import ControlledForm from './components/ControlledForm'
-import RefDemo from './components/RefDemo'
+import "./App.css";
+import UncontrolledForm from "./components/UncontrolledForm";
+import ControlledForm from "./components/ControlledForm";
+import RefDemo from "./components/RefDemo";
 
 function App() {
-  return (
-    <>
-      <UncontrolledForm/>
-      <ControlledForm/>
-      <RefDemo/>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <UncontrolledForm />
+            <ControlledForm />
+            <RefDemo />
+        </>
+    );
 }
 
-export default App
+export default App;
