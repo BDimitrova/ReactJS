8d3171181d213b204f8ccc4c8500e50aae4375a7
Boryana Dimitrova <b.dimitrova97@gmail.com>
2024-10-14 16:53:29 +0300

    useRef and forwardRef

--- Lectures/05.Forms/client/src/App.jsx
+++ Lectures/05.Forms/client/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import UncontrolledForm from "./components/UncontrolledForm";
 import ControlledForm from "./components/ControlledForm";
+import TailwindDemo from "./components/tailwindDemo";
 import RefDemo from "./components/RefDemo";
 
 function App() {
     return (
         <>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <TailwindDemo/>
             <UncontrolledForm />
             <ControlledForm />
             <RefDemo />
